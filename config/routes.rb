b57013689a1ef736997fadd3f6afbaf7d1d1f554
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2023-02-14 06:53:10 +0000

    [Add]ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,46 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'inquiries/index'
-    get 'inquiries/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-  end
-  namespace :public do
-    get 'categories/index'
-  end
-  namespace :public do
-    get 'my_characters/show'
-    get 'my_characters/edit'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
-  get 'customers/show'
-  get 'customers/edit'
-  get 'homes/top'
   scope module: 'public' do
+    # homes
+    root to: 'homes#top'
+
+    # customers
+    resources :customers, only: [:show, :edit, :update]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw'  => 'customers#withdraw'
+
+    # my_characters
+    resources :my_characters, only: [:show, :create, :edit, :update, :destroy]
+
+    # categories
+    resources :categories, only: [:index, :create, :update, :destroy]
+    post 'tag' => 'categories#tag'
+    patch 'tag_update/:id' => 'categories#tag_update', as: 'tag_update'
+    delete 'tag_destroy/:id' => 'categories#tag_destroy', as: 'tag_destroy'
+
+    # inquiries
+    resources :inquiries, only: [:create, :update]
   end
 
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ++++++++++++++++++++++++++++++++++++++++
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # ++++++++++++++++++++++++++++++++++++++++
+
+  namespace :admin do
+    # customers
+    resources :customers, only: [:index, :edit, :update]
+
+    # inquiries
+    resources :inquiries, only: [:index, :show, :create]
+  end
+
 end
