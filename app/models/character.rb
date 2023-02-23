3009273673e0abcebd03f1bfb9145064421dbaef
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2023-02-23 12:36:41 +0000

    [Add]まいきゃら一覧レイアウト

--- app/models/character.rb
+++ app/models/character.rb
@@ -11,6 +11,7 @@ class Character < ApplicationRecord
   has_one_attached :number_b_image
   has_one_attached :number_c_image
 
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -20,25 +21,25 @@ class Character < ApplicationRecord
   end
 
   def get_number_a_image(width, height)
-    unless image.attached?
+    unless number_a_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      number_a_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   	end
   	number_a_image.variant(resize_to_limit: [width, height]).processed
   end
 
   def get_number_b_image(width, height)
-    unless image.attached?
+    unless number_b_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      number_b_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   	end
   	number_b_image.variant(resize_to_limit: [width, height]).processed
   end
 
   def get_number_c_image(width, height)
-    unless image.attached?
+    unless number_c_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      number_c_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   	end
   	number_c_image.variant(resize_to_limit: [width, height]).processed
   end
