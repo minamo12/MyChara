d5670176915db88525d6873b1439a7dbffe2a2db
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2023-04-07 11:48:51 +0000

    [Add]ネタupdate機能のみ

--- app/controllers/public/materials_controller.rb
+++ app/controllers/public/materials_controller.rb
@@ -4,12 +4,28 @@ class Public::MaterialsController < ApplicationController
     @material = Material.new
     @customer = current_customer
     @materials = @customer.materials
+    if params[:material_id].present?
+      @edit_material = Material.find(params[:material_id])
+      @@edit_material = @edit_material
+    end
   end
 
   def create
     @material = Material.new(material_params)
     @material.customer_id = current_customer.id
     @material.save
+    redirect_to materials_path
+  end
+
+  def destroy
+    @material = Material.find(params[:id])
+    @material.destroy
+    redirect_to materials_path
+  end
+
+  def update
+    @material = @@edit_material
+    @material.update(material_params)
     redirect_to materials_path
   end
 
